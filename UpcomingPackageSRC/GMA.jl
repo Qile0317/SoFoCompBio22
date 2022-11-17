4aa8ca42325e1d6ccbac8a2bee4022ddac28426c
Qile <43093685+Qile0317@users.noreply.github.com>
2022-11-17 02:36:28 +0100

    Delete UpcomingPackageSRC directory

--- UpcomingPackageSRC/GMA.jl
+++
@@ -1,2 +0,0 @@
-# The genome mining algorithm proposed in my paper is purposely being withheld from this repository
-# because it is under refinement and optimization to potentially become a publication.
--- UpcomingPackageSRC/kmerGMA.jl
+++
@@ -1,9 +0,0 @@
-module kmerGMA
-    using Plots, BioSequences, FASTX, WebBlast, Distances, Statistics,
-    ProgressMeter, StringDistances, StatsBase, StatsPlots, DataFrames
-    include("simpleExplore.jl")
-    include("ExactMatch.jl")
-    include("RefGen.jl")
-    include("GMA.jl")
-    #include("GMA_Nless.jl")
-end
